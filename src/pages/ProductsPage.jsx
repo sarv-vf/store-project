9b8cb54ab20948a39e5982296c936d19f6501168
Sarvenaz Vazirifar <vazirifar.sarvenaz.1380@gmail.com>
2026-04-20 18:28:35 +0330

    fragment

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -7,16 +7,18 @@ import styles from "./ProductsPage.module.css";
 function ProductsPage() {
   const products = useProducts();
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
-        {!products.length && <Loader />}
-        {products.map((p) => (
-          <Card key={p.id} data={p} />
-        ))}
-      </div>
+    <>
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!products.length && <Loader />}
+          {products.map((p) => (
+            <Card key={p.id} data={p} />
+          ))}
+        </div>
 
-      <div></div>
-    </div>
+        <div></div>
+      </div>
+    </>
   );
 }
 
