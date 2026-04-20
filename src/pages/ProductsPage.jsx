f6d020fc1ccae7bafc344f3aa657bc735fe3faa4
Sarvenaz Vazirifar <vazirifar.sarvenaz.1380@gmail.com>
2026-04-20 14:41:38 +0330

    getting products

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,4 +1,8 @@
+import { useProducts } from "../context/ProductContext";
+
 function ProductsPage() {
+  const products = useProducts();
+  console.log(products)
   return <div>ProductsPage</div>;
 }
 
