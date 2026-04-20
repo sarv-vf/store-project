e9dec23d39f1132b57087ec80b2ccf7bbd9a3054
Sarvenaz Vazirifar <vazirifar.sarvenaz.1380@gmail.com>
2026-04-20 14:56:47 +0330

    Styles :  primary styles of ProductPage

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,9 +1,21 @@
 import { useProducts } from "../context/ProductContext";
 
+import styles from "./ProductsPage.module.css";
+
 function ProductsPage() {
   const products = useProducts();
-  console.log(products)
-  return <div>ProductsPage</div>;
+  console.log(products);
+  return (
+    <div className={styles.container}>
+      <div className={styles.products}>
+        {!products.length && <p>Loading</p>}
+        {products.map((p) => (
+          <p key={p.id}>{p.title}</p>
+        ))}
+      </div>
+      <div></div>
+    </div>
+  );
 }
 
 export default ProductsPage;
