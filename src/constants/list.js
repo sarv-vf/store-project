3bd66cb696e9aec98e44d52daafa5f384c8d5328
Sarvenaz Vazirifar <vazirifar.sarvenaz.1380@gmail.com>
2026-04-21 12:41:45 +0330

    Array of categories

---
+++ src/constants/list.js
@@ -0,0 +1,9 @@
+const categories = [
+  { id: 1, type: "All" },
+  { id: 2, type: "Electronics" },
+  { id: 3, type: "Jewelry" },
+  { id: 4, type: "Men's Clothing" },
+  { id: 5, type: "Women's Clothing" },
+];
+
+export { categories };
