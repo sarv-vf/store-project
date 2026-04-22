339731267ab5015e9957831a07f3c4cdde438d51
Sarvenaz Vazirifar <vazirifar.sarvenaz.1380@gmail.com>
2026-04-22 15:24:11 +0330

    Layout developed

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import { PiShoppingCartSimpleBold } from "react-icons/pi";
+
+import { useCart } from "../context/CartContext";
+
+import styles from "./Layout.module.css";
+
+function Layout({ children }) {
+  const [state] = useCart();
+  return (
+    <>
+      <header className={styles.header}>
+        <Link to="/products">Sugar Shop</Link>
+        <Link to="/checkout">
+          <div>
+            <PiShoppingCartSimpleBold />
+            {!!state.itemsCounter && <span>{state.itemCounter}</span>}
+          </div>
+        </Link>
+      </header>
+      {children}
+      <footer className={styles.footer}>
+        <p>Developed by Sarvenaz🤍</p>
+      </footer>
+    </>
+  );
+}
+
+export default Layout;
