07913efa0f2c99e40916b5a5f249fd1bd85a49ae
Sarvenaz Vazirifar <vazirifar.sarvenaz.1380@gmail.com>
2026-06-09 01:13:30 +0330

    fix: span itemCounters

--- src/constants/list.js
+++ src/constants/list.js
@@ -1,7 +1,7 @@
 const categories = [
   { id: 1, type: "All" },
   { id: 2, type: "Electronics" },
-  { id: 3, type: "Jewelry" },
+  { id: 3, type: "Jewelery" },
   { id: 4, type: "Men's Clothing" },
   { id: 5, type: "Women's Clothing" },
 ];
